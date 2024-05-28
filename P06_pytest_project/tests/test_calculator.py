f5c3600b21f4f93c97f3b09b45073ed1607db455
vanessachhang <23030977@myrp.edu.sg>
2024-05-28 16:03:14 +0800

    inital commit

---
+++ P06_pytest_project/tests/test_calculator.py
@@ -0,0 +1,63 @@
+from calculator.calculator import Calculator
+import pytest
+
+class TestCalculator:
+    def test_add(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.add(a, b)
+
+        # assert
+        expected = 5555
+        assert result == expected
+
+    def test_subtract(self):
+        # arrange 
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+         # act
+        result = cal.subtract(a, b)
+
+        # assert
+        expected = 3087
+        assert result == expected
+
+    def test_multiply(self):
+        # arrange 
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+         # act
+        result = cal.multiply(a, b)
+
+        # assert
+        expected = 5332114
+        assert result == expected
+
+    def test_divide(self):
+        # arrange 
+        a = 4
+        b = 2
+        cal = Calculator()
+
+         # act
+        result = cal.divide(a, b)
+
+        # assert
+        expected = 2
+        assert result == expected
+        
+    def test_divide_by_zero(selef):
+        a =4321
+        b =0
+        cal = Calculator()
+        
+        with pytest.raises(ZeroDivisionError):
+            cal.divide(a,b)
